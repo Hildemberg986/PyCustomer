c725e00552b73917f2479885eed6b9c8607939b8
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-21 18:09:20 -0300

    creating code for user search

--- functions/search.py
+++ functions/search.py
@@ -1,2 +1,16 @@
 def colar():
-    print('colar')
+    import pickle
+
+    Users = {}
+
+    try:
+     arqUser = open("Users.dat", "rb")
+     Users = pickle.load(arqUser)
+     arqUser.close()
+    except:
+     arqUser = open("Users.dat", "wb")
+     arqUser.close()
+    id_user = len(Users.keys())
+
+for cpf in Users:
+    print(cpf)

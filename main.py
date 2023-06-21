68408eafb09f25ce9d1dd332f0ced97e83531579
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-21 18:06:10 -0300

    imports and tests

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from functions.create import *
 from functions.search import *
 from functions.delete import *
 from functions.clear import *
-clear()
-line = "\n"
+from functions.datebase import *
+
+
+
+clear() 
 add = create_user()
-clear()
-olhar = open("User.txt", "r")
-print(olhar.readlines())
+datebase(add)

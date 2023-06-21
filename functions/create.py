cb7913275a39eeca31cbe2fdc4ad0f1068b08317
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-21 18:08:04 -0300

    attempted non-registration of users with the same CPF

--- functions/create.py
+++ functions/create.py
@@ -1,21 +1,29 @@
 def create_user():
     from functions.clear import clear
+    import pickle
+
+    Users = {}
+
+    try:
+     arqUser = open("Users.dat", "rb")
+     Users = pickle.load(arqUser)
+     arqUser.close()
+    except:
+     arqUser = open("Users.dat", "wb")
+     arqUser.close()
+    id_user = len(Users.keys())
+
     name = input("Nome do novo usuario... ")
     clear()
     cont = 0
-    Users = ["123"]
     while cont == 0:
         cpf = input("Digite seu cpf... ")
-        if cpf in Users:
+        print('000' in Users.values())
+        if cpf in Users.values():
             clear()
             print("CPF já cadastrado em outro usuário")
         else:
             print('Novo usuário cadastrado')
             cont = 1
-            NewUser = [name, cpf]
-            InputUser = open("User.txt", "a")
-            for item in NewUser:
-                InputUser.write(item + '\n')
-            InputUser.write('\n')
-            InputUser.close()
-    return name; cpf
+    user = [name, cpf]
+    return user

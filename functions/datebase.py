a109e0a1c57536c7fe369e3722e4df40e75003de
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-22 01:58:26 -0300

    created a new ip generator

--- functions/datebase.py
+++ functions/datebase.py
@@ -1,8 +1,12 @@
 def datebase(add):
     import pickle
-
+    from datetime import date
+    
+    data_atual = date.today()
     Users = {}
-
+    cpf_user = add[1]
+    initial_cpf = cpf_user[0:3]
+    end_cpf = cpf_user[6:9]
     try:
      arqUser = open("Users.dat", "rb")
      Users = pickle.load(arqUser)
@@ -10,7 +14,7 @@ def datebase(add):
     except:
      arqUser = open("Users.dat", "wb")
      arqUser.close()
-    id_user = len(Users.keys()) 
+    id_user = '{}000{}{}'.format (data_atual.year, initial_cpf, end_cpf) 
 
     Users[id_user] = add
     

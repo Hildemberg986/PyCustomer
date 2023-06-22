e18ace54414258cbd0ced845904826fa33aa94f8
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-22 01:57:24 -0300

    improved search function created

--- functions/search.py
+++ functions/search.py
@@ -1,16 +1,39 @@
-def colar():
+def search_user():
     import pickle
-
+    from functions.clear import clear
+    
+    
     Users = {}
-
     try:
-     arqUser = open("Users.dat", "rb")
-     Users = pickle.load(arqUser)
-     arqUser.close()
+        arqUser = open("Users.dat", "rb")
+        Users = pickle.load(arqUser)
+        arqUser.close()
     except:
-     arqUser = open("Users.dat", "wb")
-     arqUser.close()
-    id_user = len(Users.keys())
+        arqUser = open("Users.dat", "wb")
+        arqUser.close()
+    fim = 0
+    while fim != "0":
+        clear()
+        searchitem = input('Digite o cpf ou nome do usuario que deseja buscar... ')
+        clear()
+        for key, user in Users.items():    
+            if searchitem in user:
+                print()
+                User ='''
+        id: {} 
+        Nome: {} 
+        CPF: {} 
+        E-mail: {}'''.format (key,user[0],user[1],user[2]) 
+                print(User)       
+                break
+            User = ''
 
-for cpf in Users:
-    print(cpf)
+        if User == '':
+            escolha = input('Usuario nao encontrado, deseja buscar novanmente ? s/n ')
+            if escolha == 'n':
+                break
+        elif user != "":
+            print()
+            print()
+            input('Precione enter para continuar...')
+            break

07e65434efa16bcd2fa070a87253817a4ffc1741
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-29 15:29:57 -0300

    modifying the way of saving in the local datebase

--- functions/datebase.py
+++ functions/datebase.py
@@ -1,24 +1,9 @@
-def datebase(add):
+def datebase(user, users):
     import pickle
-    from datetime import date
-    
-    data_atual = date.today()
-    Users = {}
-    cpf_user = add[1]
-    initial_cpf = cpf_user[0:3]
-    end_cpf = cpf_user[6:9]
-    try:
-     arqUser = open("Users.dat", "rb")
-     Users = pickle.load(arqUser)
-     arqUser.close()
-    except:
-     arqUser = open("Users.dat", "wb")
-     arqUser.close()
-    id_user = '{}000{}{}'.format (data_atual.year, initial_cpf, end_cpf) 
+    cpf_user = user[1]
 
-    Users[id_user] = add
+    users[cpf_user] = user
     
-    arqUser = open("Users.dat", "wb")
-    pickle.dump(Users , arqUser)
+    arqUser = open("users.dat", "wb")
+    pickle.dump(users , arqUser)
     arqUser.close()
-    print (Users)

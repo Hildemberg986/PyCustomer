62300fa3509f358e157ec04790aea63e94223be7
Hildemberg Eling <hildembergeling@gmail.com>
2023-06-29 15:32:29 -0300

    modifying the way of checking CPF and improvement of programming logic

--- functions/create.py
+++ functions/create.py
@@ -1,42 +1,35 @@
-def create_user():
-    from functions.duplicatecpfchecker import duplicate_cpf_checker
-    from functions.checker import validar_cpf
+def create_user(users):
+    from functions.datebase import datebase
+    from functions.checkerCPF import validar_cpf
     from functions.clear import clear
-    import pickle
     import time
-
-    Users = {}
-
-    try:
-     arqUser = open("Users.dat", "rb")
-     Users = pickle.load(arqUser)
-     arqUser.close()
-    except:
-     arqUser = open("Users.dat", "wb")
-     arqUser.close()
     
-
-    name = input("Nome do novo usuario... ")
+    name = input("Nome do novo usuário... ")
     clear()
-    cont = 0
-    while cont == 0:
+    
+    cpf = input("Digite seu cpf... ")
+    clear()
+    
+    checker = validar_cpf(cpf, users)
+    
+    while checker  == False or checker == 'double': 
+        if checker == False:
+            print('cpf invalido')
+            print()
+        elif checker == 'double':
+            print('cpf cadastrado em outro usuário')
+            print()
+            
         cpf = input("Digite seu cpf... ")
-        cpf = ''.join(filter(str.isdigit, cpf))
-        checker_cpf = validar_cpf(cpf)
-        if checker_cpf:
-            if duplicate_cpf_checker(cpf):
-                clear()
-                print("CPF já cadastrado em outro usuário")
-            else:
-                break
-        else:
-            clear()
-            print('CPF inválido!!')
+        checker = validar_cpf(cpf, users)
     clear()
-    email = input("Digite seu e-mail... ")
+    
+    email = input('Digite seu e-mail... ')
     clear()
-    print('Novo usuario cadastrado')
-    time.sleep(3)
-
+    
     user = [name, cpf, email]
-    return user
+    
+    datebase(user,users)
+    
+    print ('Novo usuário cadastrado!')
+    time.sleep(1)

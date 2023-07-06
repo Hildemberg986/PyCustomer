fa273959db5854398a5537657265fbe6d8436e62
Hildemberg Eling <hildembergeling@gmail.com>
2023-07-06 15:14:13 -0300

    commenting the code and implementing the sales save

--- functions/datebase.py
+++ functions/datebase.py
@@ -1,9 +1,33 @@
-def datebase(user, users):
+#Função para dicionarios no banco de dados 
+def datebase(sale,user,sales, users, option):
+    from datetime import datetime
     import pickle
-    cpf_user = user[1]
 
-    users[cpf_user] = user
-    
-    arqUser = open("users.dat", "wb")
-    pickle.dump(users , arqUser)
-    arqUser.close()
+    #Define qual banco de dados salvar 
+    if option == "user" or 'update user':
+
+        #Extrai o CPF do cliente para aser usado como key no dicionario
+        cpf_user = user[1]
+
+        #Implementa o novo user no dicionario users
+        users[cpf_user] = user
+
+        #Salva o dicionario no arquivo .dat em binario
+        arqUser = open("users.dat", "wb")
+        pickle.dump(users , arqUser)
+        arqUser.close()
+    elif option == "sale":
+
+        #Puxa a data atual da biblioteca datetime
+        data_atual = datetime.today()
+
+        #Gera a key usando a data e hora 
+        key = data_atual.strftime('''%d%m%Y%H%M%S''')
+
+        #Implementa a nova sale no dicionario sales
+        sales[key] = sale
+
+        #Salva o dicionario no arquivo .dat em binario
+        arqSales = open("sales.dat", "wb")
+        pickle.dump(sales , arqSales)
+        arqSales.close()
